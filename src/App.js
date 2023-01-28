babd1a968c1cb51a89d15786cbd4d7f933fc1833
anurag9012 <88288329+anurag9012@users.noreply.github.com>
2023-01-28 12:54:10 +0530

    csb with github steps to connect csb with github reposiory , and then comit changes etc

--- src/App.js
+++ src/App.js
@@ -4,7 +4,18 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <ol>
+        <li>code sandbox p jake create new react kiya</li>
+        <li>git vale symbol ko click karke new repository banayi</li>
+        <li>project karke kuch open hua samanahi aaya, ignore it</li>
+        <li>github p reposiiory mein gaya , fir link ko click kiya </li>
+        <li>ab code sand box mein workspace khul gayi</li>
+        <li>ab git vale symbol p gaya to link sandbox ka option aaya</li>
+        <li>usko click kark , ab commit karne ka option aagaya and ,</li>
+        <li>
+          then there is even one option to work on main branch or a new branch
+        </li>
+      </ol>
     </div>
   );
 }
